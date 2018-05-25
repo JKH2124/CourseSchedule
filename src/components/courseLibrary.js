03a9827c1231b62c2d256f23c41617542cc6bc7f
Joshua Harris <jnharris208@gmail.com>
2018-05-25 12:02:17 -0600

    added add and remove button to course library component andset up add course action

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {connect, dispatch } from 'react-redux';
-import { fetchCourses } from '../actions';
+import { fetchCourses, addCourse } from '../actions';
 
 class CourseLibrary extends Component {
 
@@ -21,6 +21,9 @@ class CourseLibrary extends Component {
                 <div className="course_title-container">
                     <div className="course_title">{course.title}</div>
                 </div>
+                <a>arrow</a>
+                <a className={`action ${course.enrolled ? 'hide-content' : 'show-content'}`} onClick={() => this.props.addCourse(course)}>add</a>
+                <a className={`action ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove</a>
             </div>
             <div className="course_description">
                 <h6 className="course_description-title">Course Description</h6>
@@ -49,6 +52,12 @@ function mapDispatchToProps(dispatch) {
     return {
         fetchCourses:()=> {
         dispatch(fetchCourses())
+        },
+        addCourse:(course)=> {
+            dispatch(addCourse(course))
+        },
+        removeCourse:(course)=> {
+                dispatch(removeCourse(course))
         }
     }
 }
