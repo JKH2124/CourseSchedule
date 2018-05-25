3c6b7e0a5e9074a84015168018431e1358aa0af4
Joshua Harris <jnharris208@gmail.com>
2018-05-25 14:40:44 -0600

    added arrow animations and styles

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect, dispatch } from 'react-redux';
 import { fetchCourses, addCourse, removeCourse, toggleDescription } from '../actions';
+import AnimateHeight from 'react-animate-height'
 
 class CourseLibrary extends Component {
 
@@ -21,14 +22,20 @@ class CourseLibrary extends Component {
                 <div className="course_title-container">
                     <div className="course_title">{course.title}</div>
                 </div>
-                <a onClick={() => this.props.toggleDescription(course)}>arrow</a>
+                <a className={`course_arrow ${course.open ? null : 'course_arrow-close'}`} onClick={() => this.props.toggleDescription(course)}></a>
                 <a className={`action ${course.enrolled ? 'hide-content' : 'show-content'}`} onClick={() => this.props.addCourse(course)}>add</a>
                 <a className={`action ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove</a>
             </div>
-            <div className={`course_description ${course.open ? 'show-content' : 'hide-content'} `}>
+        <AnimateHeight
+            duration={ 300 }
+            height={ course.open ? 'auto' : '0' }
+        >
+            <div className={`course__description`}>
                 <h6 className="course_description-title">Course Description</h6>
                 <p>{course.description}</p>
             </div>
+        </AnimateHeight>
+        
         </li>
         )
     }
