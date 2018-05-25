8f587c23bf683736f572a96fb01a460d43734023
Joshua Harris <jnharris208@gmail.com>
2018-05-25 16:06:02 -0600

    added hover and list styles

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -13,8 +13,8 @@ class Schedule extends Component {
     renderCourse(course) {
         return (
             <div key={this.props.courses.indexOf(course)} className={`slot ${course.enrolled ? 'slot_course' : 'slot_empty'}`}>
-                <div className="slot_title">{course.enrolled ? course.title : 'Empty Slot'}</div>
-                <a className={`action slot_remove ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove course</a>
+                <div>{course.enrolled ? course.title : 'Empty Slot'}</div>
+                <a className={`action slot_remove`} onClick={() => this.props.removeCourse(course)}>remove course</a>
             </div>
         )
     }
