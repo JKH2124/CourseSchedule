94542f41496da5655b29103463f31c6a83ef10f5
Joshua Harris <jnharris208@gmail.com>
2018-05-29 08:43:43 -0600

    added a course limit to our ADD_COURSE reducer for our schedule component

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -15,7 +15,6 @@ class Schedule extends Component {
     }
 
     renderCourse(course) {
-        console.log(this.state.enrolled.indexOf(course));
         return (
             <div key={this.state.enrolled.indexOf(course)} className={`slot ${course.enrolled ? 'slot_course' : 'slot_empty'}`}>
                 <div>{course.enrolled ? course.title : 'Empty Slot'}</div>
